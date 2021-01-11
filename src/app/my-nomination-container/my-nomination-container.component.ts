f4208179b6222d0aaeabd5f7ad3f9d42e5c17b08
Ganesh <ganesh.bhogle88@gmail.com>
2021-01-11 16:16:35 -0500

    image fix

--- src/app/my-nomination-container/my-nomination-container.component.ts
+++ src/app/my-nomination-container/my-nomination-container.component.ts
@@ -1,3 +1,4 @@
+import { LocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
@@ -12,8 +13,9 @@ export class MyNominationContainerComponent implements OnInit {
 
   programId : string = "";
   fullImagePath = '/assets/images';
+  imgVisible:boolean = false;
 
-  constructor(private route: ActivatedRoute,private router: Router,
+  constructor(private route: ActivatedRoute,private router: Router,private url:LocationStrategy,
     public dialog: MatDialog,) {
      
     console.log(this.router.url); 
@@ -22,7 +24,13 @@ export class MyNominationContainerComponent implements OnInit {
       (event: any) => {
         if (event instanceof NavigationEnd) {
           console.log('this.router.url', this.router.url);
+          console.log(this.url.path());
           let isLoginSuccess = sessionStorage.getItem("isLoginSuccessfull");
+          if((this.url.path() == "/")){
+            this.imgVisible = true;
+          }else{
+            this.imgVisible = false;
+          }
           
           if((this.router.url.indexOf('nomination') <= 0 || this.router.url.indexOf('message') <= 0) && !isLoginSuccess){
             const dialogRef = this.dialog.open(LoginComponent);
